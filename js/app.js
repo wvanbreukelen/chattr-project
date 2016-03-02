951b0b5cc749490c1e64d625402bee59420c82bf
Wiebe van Breukelen <wvanbreukelen@users.noreply.github.com>
2016-03-02 09:43:39 +0100

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,39 @@
+function register()
+{
+	// AJAX stuff
+}
 
+function validateRegisterForm()
+{
+	var username  = $('#username');
+	var password  = $('#password');
+	var email     = $('#email');
+	var firstName = $('#firstName');
+	var lastName  = $('#lastName');
+
+	error = false;
+
+	if (username.length < 6 || username.length > 12)
+	{
+		error = true;
+	}
+
+	if (password.length < 7 || password.length > 12) 
+	{
+		error = true;
+	}
+
+	if (firstName.length < 6 || firstName.length > 12)
+	{
+		error = true;
+	}
+	
+	if (lastName.length < 6 || lastName.length > 12)
+	{
+		error = true;
+	}
+	
+	$('#errorBox').css('hidden', false);
+	$('#errorBox').html("<div id='alert'>Vul alle verplichte velden in!</div>");
+
+}
