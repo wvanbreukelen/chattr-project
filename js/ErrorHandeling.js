33d7eaaabc5b2e6df564722b7f60a23114fa5f6b
rollerorange <joery1999@gmail.com>
2016-03-04 09:52:56 +0100

    Added files via upload

---
+++ js/ErrorHandeling.js
@@ -0,0 +1,21 @@
+if(error==1)
+{
+	window.prompt("Dit emailadres is ongeldig.)
+}
+else if(error==2)
+{
+	window.prompt("gebruikersnaam lengte is niet tussen de 3 en 12 tekens.")
+}
+else if(error==3)
+{
+	window.prompt("opgegeven wachtwoord is niet tussen de 7 en 20 tekens.")
+}
+else if(error==4)
+{
+	window.prompt("opgegeven voornaam is niet tussen de 2 en 22 tekens.")
+}
+else if(error==5)
+{
+	window.prompt("opgegeven achternaam is niet tussen de 2 en 22 tekens.")
+}
+
