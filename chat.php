1e218499ddf83851e2ef3929099b34c54e6ffc21
Wiebe van Breukelen <wvanbreukelen@users.noreply.github.com>
2016-03-09 10:25:46 +0100

    New upload

--- chat.php
+++ chat.php
@@ -2,6 +2,19 @@
 
 require('ajax/init.php');
 
+$redirect->redirectIfLoggedout();
+
+if (isset($_POST['message']))
+{
+	$message      = html_entity_decode($_POST['message']);
+	$curTimestamp = time();
+
+	if (!$validator->validateString($message, 1, 200))
+	{
+		exit("1");
+	}
+	
+}
 
 ?>
 
@@ -17,21 +30,24 @@ require('ajax/init.php');
 		</div>
 
 		<div class="chattr-content">
-			<h2>{$ChatName}</h2>
+			<h2>Algemene Chat</h2>
 
 			<span>
 				<textarea readonly name="chatbox" id="chatbox" rows="13" cols="90">[$time] $message&#13;&#10;[$time] $message&#13;&#10;[$time] $message&#13;&#10;[$time] $message</textarea>
 				<br />
 				<input type="text" name="chatMessage" id="chatMessage" style="margin-right: 20px; margin-bottom: 10px; width: 650px;"><br />
-				<button class="btn-success" name="sendChatMessage" id="sendChatMessage">Verstuur!</button>
+				<button onclick="sendChatMessage()" class="btn-success" name="sendChatMessage" id="sendChatMessage">Verstuur!</button>
 			</span>
 			<div class="userBox">
-				Test
+				
 			</div>
 		</div>
 
 		<div class="chattr-footer">
 			<p>Door Joery Hoegee en Wiebe van Breukelen</p>
 		</div>
+
+		<script src="js/jquery.js"></script>
+		<script src="js/app.js"></script>
 	</body>
 </html>
--- js/app.js
+++ js/app.js
@@ -1,6 +1,64 @@
 function register()
 {
 	// AJAX stuff
+
+	$.post("register.php", { username: $('#username').val(), password: $('#password').val(), firstName: $('#firstName').val(), lastName: $('#lastName').val(), email: $('#email').val() }, function(data) {
+		$('#message').css('display', 'block');
+		
+		console.log(data);
+
+		if(data == 1)
+		{
+			error = "Dit emailadres is ongeldig.";
+		} else if (data == 2) {
+			error = "Gebruikersnaam lengte is niet tussen de 3 en 12 tekens.";
+		} else if (data == 3) {
+			error = "Opgegeven wachtwoord is niet tussen de 7 en 20 tekens.";
+		} else if (data == 4) {
+			error = "Opgegeven voornaam is niet tussen de 2 en 22 tekens.";
+		} else if (data == 5) {
+			error = "Opgegeven achternaam is niet tussen de 2 en 22 tekens.";
+		}
+
+		if (typeof error === "undefined" || data == 0)
+		{
+			$('#message').html("Successvol registreert, je wordt nu doorgestuurd...");
+			window.location = "chat.php";
+		} else {
+			$('#message').html(error);
+		}
+		
+	});
+
+
+}
+
+function login()
+{
+
+	$.post("login.php", { username: $('#username').val(), password: $('#password').val() }, function(data) {
+		$('#message').css('display', 'block');
+		
+		console.log(data);
+
+		if (data == 1)
+		{
+			error = "De opgegeven gebruikersnaam lengte is niet tussen de 3 en 12 tekens!";
+		} else if (data == 2) {
+			error = "Het opgegeven wachtwoord is niet tussen de 7 en 20 tekens!";
+		} else if (data == 3) {
+			error = "Er is geen account bij de ingevoerde gegevens gevonden!";
+		}
+
+		if (typeof error === "undefined" || data == 0)
+		{
+			$('#message').html("Successvol ingelogd, je wordt nu automatisch doorgestuurd...");
+			setTimeout(function(){ window.location = "chat.php"; }, 3000);
+		} else {
+			$('#message').html(error);
+		}
+		
+	});
 }
 
 function validateRegisterForm()
@@ -36,4 +94,14 @@ function validateRegisterForm()
 	$('#errorBox').css('hidden', false);
 	$('#errorBox').html("<div id='alert'>Vul alle verplichte velden in!</div>");
 
+}
+
+function sendChatMessage()
+{
+	var message = $('#chatMessage').val();
+
+
+	$.post("chat.php", { message: message }, function(data) {
+		console.log(data);
+	});
 }
